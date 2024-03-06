40f73f7f6b5568a4303f7d526a56d7969081d5a0
gevin <gmadharh@uoguelph.ca>
2024-03-06 15:18:38 -0500

    Add background image and style to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div
+      style={{
+        backgroundImage: `url(${`https://picsum.photos/1920/1080`})`,
+        backgroundSize: `cover`,
+        backgroundPosition: `center`,
+        backgroundRepeat: `no-repeat`,
+        width: `100vw`,
+        height: `100vh`,
+        margin: 0,
+        padding: 0,
+      }}
+    ></div>
   )
 }
 
