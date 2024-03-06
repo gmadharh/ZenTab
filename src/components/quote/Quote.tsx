c9a7a43c77d2bf3e8df2e8a41e4144c70084d4eb
gevin <gmadharh@uoguelph.ca>
2024-03-06 16:25:36 -0500

    Add random quote functionality

--- src/components/quote/Quote.tsx
+++ src/components/quote/Quote.tsx
@@ -1,9 +1,23 @@
 import './quote.css'
+import { useEffect, useState } from 'react'
 
 function Quote() {
+  const [quote, setQuote] = useState('')
+  const [author, setAuthor] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.content)
+        setAuthor(data.author)
+      })
+  }, [])
+
   return (
     <div>
-      <h1>Quote</h1>
+      <h1>{quote}</h1>
+      <h3>{author}</h3>
     </div>
   )
 }
